b4fb110a91c973f6fca8fc6153a614ad63767526
Peros <pierre_w@gmx.de>
2018-01-23 12:34:38 +0100

    - added get inventory from db example

--- Python/db-smartcart-test.py
+++ Python/db-smartcart-test.py
@@ -7,4 +7,11 @@ cursor.execute('SELECT * FROM products')
 for row in cursor:
     print('row = %r' % (row,))
 
+print('----------------')
+cursor.execute('SELECT * FROM products RIGHT JOIN (SELECT ProductId, Count, Price FROM inventory where BrandId = 1) as Inventory on (products.Id = Inventory.ProductId)')
+
+for row in cursor:
+    print('row = %r' % (row,))
+
+
 conn.close()
