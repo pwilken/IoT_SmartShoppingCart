c24c4784a61013ba5d6add2edfa88fa3d7ee08dc
daweitz <weitzdaniel1@googlemail.com>
2018-02-03 12:48:50 +0100

    Added endless loop and farewell message

--- Python/ReadCart.py
+++ Python/ReadCart.py
@@ -13,35 +13,38 @@ def main():
 
     lcd = MyLCD.MyLCD()
     reader = SimpleMFRC522()
+    while(True):
+        checkedOut=False
+        slist = ShoppingCart.ShoppingCart(conn)
+        shoppingListSum=0
+        shoppingListSumString = str(shoppingListSum) + ",-"
 
-    checkedOut=False
-    slist = ShoppingCart.ShoppingCart(conn)
-    shoppingListSum=0
-    shoppingListSumString = str(shoppingListSum) + ",-"
+        while(checkedOut==False):
+            try:
+                lcd.lcd_print("Summe",1)
+                lcd.lcd_print(shoppingListSumString,2)
 
-    # Initialise display
+                id,itemName=reader.read()
+                if(id==48700008907): #checkout if id matches checkout tag, white card atm
+                    checkedOut=True
+                else:
+                    temp = Item.Item(itemName, id)
+                    slist.addItem(temp)
+                    shoppingListSum = slist.getListSum()
+                    shoppingListSumString = str(shoppingListSum) + ",-"
+                    sleep(1)
+            except KeyboardInterrupt:
+                pass
 
-    while(checkedOut==False):
+            finally:
+                GPIO.cleanup()
+                lcd.cleanup()
         try:
-            lcd.lcd_print("Summe",1)
-            lcd.lcd_print(shoppingListSumString,2)
-
-            id,itemName=reader.read()
-            if(id==48700008907): #checkout if id matches checkout tag, white card atm
-                checkedOut=True
-            else:
-                temp = Item.Item(itemName, id)
-                slist.addItem(temp)
-                shoppingListSum = slist.getListSum()
-                shoppingListSumString = str(shoppingListSum) + ",-"
-                sleep(1)
-        except KeyboardInterrupt:
-            pass
-
+            lcd.lcd_print("Vielen Dank für",1)
+            lcd.lcd_print("Ihren Einkauf!",2)
         finally:
-            GPIO.cleanup()
-            lcd.cleanup()
+            lcd.cleanup
     conn.close()
 
 if __name__ == "__main__":
-	main()
+    main()
